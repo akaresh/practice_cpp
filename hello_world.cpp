bde850ecd7450788ea54b66cc4c469306389676f
Amalia <58994466+akaresh@users.noreply.github.com>
2021-03-05 15:16:56 -0800

    intro to c++

---
+++ hello_world.cpp
@@ -0,0 +1,20 @@
+// first C++ program
+
+#include <iostream>
+// # = prerocessor directive
+// inluclude = add directives of a given library = iostram library
+// <stream> = look in the current directory first, then look in the
+// directory where the standard libraries are kept
+// "" = look in the current directory first, then look in the directory
+//where the standard libraries are kept
+// basically changing <> to "" = it can make a difference
+// <> - goes straight to the standard directory
+// "" - goes to the current directory first and only then to the standard
+
+int main() {
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
+//#include <iostream>
+
