a8f2c3d151ed5e97adf2a0762b3b134cdf3caf07
Amalia <58994466+akaresh@users.noreply.github.com>
2021-03-05 15:49:56 -0800

    simple practice program

---
+++ p1.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+// program takes in name and favorite year
+int main()
+{
+    int year = 0;
+    int age = 0;
+    std::string name = "";
+    //declerating variables above
+
+    std::cout << "What is your name?";
+    std::cout << "\n";
+    std::cin >> name;
+    std::cout <<"What year is your favority? \n";
+    std::cin >> year;
+
+    std::cout << "Your name: " << name << "\n";
+    std::cout << "Favorite year: " << year << "\n";
+    return 0;
+}
+
+//cin is an object of class istream = accept the input
