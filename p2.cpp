ffd65a6533a765dc8ef108ea7a48976c103cc4b4
Amalia <58994466+akaresh@users.noreply.github.com>
2021-03-05 16:19:30 -0800

    p2

---
+++ p2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int variableName = 323
+    cout<<variableName; // just printouts the value
+    //strings need ""
+    cout << "The value of integer is" << variableName
+    return 0;
+}
